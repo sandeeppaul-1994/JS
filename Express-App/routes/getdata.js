47054afe57001213fa857d418de2c18d4b3fc0ad
Administrator <shruthicr.11@gmail.com>
2018-10-16 10:08:09 +0000

    added logger

--- Express-App/routes/getdata.js
+++ Express-App/routes/getdata.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var app     = express();
 app.use(express.static(__dirname + '/views'));
 var mysql = require('mysql');
+var logger = require('../logger').Logger;
 var date="";
 
 var con = mysql.createConnection({
@@ -16,6 +17,7 @@ var con = mysql.createConnection({
 con.connect(function(err) { 
   if (err) throw err;
   console.log ("connected in get data js");
+  logger.info("Connected in get data js");
 });
 
 
@@ -23,12 +25,15 @@ router.post('/',function(req,res){
     var username=req.body.user;
     var query="select max(date) as date from User where name='"+username+"'";
     console.log("query"+query);
+    logger.info("query"+query);
 
     con.query(query, function (err, result, fields) {
       if (err) throw err;
       console.log('The solution is: ', result[0].date);
+      logger.info('The solution is: ', result[0].date);
       date_value=Date(result[0].date);
       console.log(date_value);
+      logger.info('date value is :'+ date_value);
     });
     res.send({name:username, date:date_value});
   });
--- Express-App/routes/login.js
+++ Express-App/routes/login.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router();
 var app     = express();
+var logger = require('../logger').Logger;
 //var con = require('../con.js');
 app.use(express.static(__dirname + '/views'));
 
@@ -18,6 +19,7 @@ var con = mysql.createConnection({
 con.connect(function(err) { 
   if (err) throw err;
   console.log ("connected");
+  logger.info("Connected in login js");
 });
 
 
@@ -31,16 +33,20 @@ router.post('/',function(req,res){
     var password=req.body.password;
     var date = new Date().getTime();
     console.log('date',date);
+    logger.info('date::',date);
     // var date = mydate('date');
     var query="INSERT INTO User (name,password,date) VALUES ('"+user_name+"','"+password+"','"+date+"')";
     console.log("query"+query);
+    logger.info("query::"+query);
 
     con.query(query, function (err, result, fields) {
       if (err) throw err;
       console.log('The solution is: ', result);
+      logger.info('The solution is: ', result);
       //con.end();
     });
     console.log("User name = "+user_name+", password is "+password);
+    logger.info("User name = "+user_name+", password is "+password);
     
     
     if(name.indexOf(user_name) >= 0 && namePass[user_name] === password){
